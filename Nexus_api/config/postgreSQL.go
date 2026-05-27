ef068f9723d9a7094ea398c4d4ffc2c934d39a74
Matheus Mikio <matheus.mikio@interfocus.com.br>
2026-05-27 12:05:55 -0300

    Add configuration and PostgreSQL initialization for database connection

---
+++ Nexus_api/config/postgreSQL.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func initPostgreSQL(cfg Config) (*gorm.DB, error) {
+	dbConnection := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		cfg.DBHost, cfg.DBPort, cfg.DBUser, cfg.DBPassword, cfg.DBName, cfg.DBSSLMode)
+
+	db, err := gorm.Open(postgres.Open(dbConnection), &gorm.Config{})
+	if err != nil{
+		return nil, err
+	}
+
+	err := db.AutoMigrate(
+		&models.User{},
+		&models.Project{},
+		&models.Task{}
+	)
+
+	if err != nil{
+		return nil, err
+	}
+
+	return db, nil
+}
